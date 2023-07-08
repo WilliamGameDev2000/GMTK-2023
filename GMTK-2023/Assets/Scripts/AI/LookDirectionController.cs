b1fde9d8525f235205bcc5911d4862a60e6aeca2
William <willgamedev@gmail.com>
2023-07-08 20:13:26 +0100

    randomly looks away from the table now

--- GMTK-2023/Assets/Scripts/AI/LookDirectionController.cs
+++ GMTK-2023/Assets/Scripts/AI/LookDirectionController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class LookDirectionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    bool is_looking;
+
+    float initial_forwardY;
+
+    private void Start()
     {
-        
+        initial_forwardY = transform.eulerAngles.y;
+
+        StartCoroutine("SwapLook", .75f);
+    }
+
+    IEnumerator SwapLook(float period)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(period);
+            if(Random.value >= 0.5)
+            {
+                if (!is_looking)
+                {
+                    if (Random.value >= 0.5)
+                    {
+                        transform.LeanRotateAround(new Vector3(0, transform.position.y, 0), 150, .3f);
+                    }
+                    else
+                    {
+                        transform.LeanRotateAround(new Vector3(0, transform.position.y, 0), -150, .3f);
+                    }
+                    is_looking = true;
+                }
+            }
+            else
+            {
+                if (is_looking)
+                {
+                    transform.LeanRotateY(initial_forwardY, 0.3f);
+                    //transform.LeanRotateAround(new Vector3(0, transform.position.y, 0), -150, .3f);
+                    is_looking = false;
+
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
