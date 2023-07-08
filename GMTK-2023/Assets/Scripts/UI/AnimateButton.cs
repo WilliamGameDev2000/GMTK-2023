a71324b5fed6d8d4267e2872408ab728ac92b56c
William <willgamedev@gmail.com>
2023-07-08 17:08:01 +0100

    integrated menu to npc branch

--- GMTK-2023/Assets/Scripts/UI/AnimateButton.cs
+++ GMTK-2023/Assets/Scripts/UI/AnimateButton.cs
@@ -14,12 +14,16 @@ public class AnimateButton : MonoBehaviour
     
     public void ComeApart()
     {
+        audiomanager.instance.Play("cardshuffle1");
+        audiomanager.instance.Play("cardshuffle2");
         BackgroundCard1.transform.LeanMoveLocalY(moveAmount, timings);
         BackgroundCard2.transform.LeanMoveLocalY(-moveAmount, timings);
     }
 
     public void ComeTogether()
     {
+        audiomanager.instance.Play("cardshuffle1");
+        audiomanager.instance.Play("cardshuffle2");
         BackgroundCard1.transform.LeanMoveLocalY(0, timings);
         BackgroundCard2.transform.LeanMoveLocalY(0, timings);
     }
