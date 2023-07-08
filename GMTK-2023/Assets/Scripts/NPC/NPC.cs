7f99e63a3453b5d93b6464c71a3d7ce5c0817784
William <willgamedev@gmail.com>
2023-07-08 10:53:34 +0100

    suspicion meter value tied to what they see
    will need to add more clever things to the detection, say use tags and invisible colliders to fill the correct meters perhaps?

--- GMTK-2023/Assets/Scripts/NPC/NPC.cs
+++ GMTK-2023/Assets/Scripts/NPC/NPC.cs
@@ -7,6 +7,8 @@ public class NPC : MonoBehaviour
 
     [SerializeField] NPCSO data;
 
+    [SerializeField] GameObject ViewCone;
+
     float Current_suspicion;
     float Current_trust;
     // Start is called before the first frame update
@@ -19,17 +21,23 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     protected void Update()
     {
-        AddSuspicion(-.00051f);
-        Mathf.Clamp(GetSuspicion(), 0, 1);
+        if(ViewCone.GetComponent<FieldOfView>().visibleTargets.Count != 0)
+        {
+            AddSuspicion(.00025f);
+        }
+
+        AddSuspicion(-.000125f);
     }
 
     public void AddSuspicion(float sus)
     {
         Current_suspicion += sus;
+        Current_suspicion = Mathf.Clamp(Current_suspicion, 0, 1);
     }
     public void AddTrust(float trust)
     {
         Current_trust += trust;
+        Current_trust = Mathf.Clamp(Current_trust, 0, 1);
     }
 
     public float GetSuspicion()
