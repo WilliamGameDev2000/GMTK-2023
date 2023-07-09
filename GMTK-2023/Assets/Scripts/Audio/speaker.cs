669f8339243c3c943982675f355ac049c020aad8
William <willgamedev@gmail.com>
2023-07-09 10:05:24 +0100

    added background sounds

--- GMTK-2023/Assets/Scripts/Audio/speaker.cs
+++ GMTK-2023/Assets/Scripts/Audio/speaker.cs
@@ -4,9 +4,48 @@ using UnityEngine;
 
 public class speaker : MonoBehaviour
 {
+
+    bool inMenu = true;
+
+    public static speaker instance;
     // Start is called before the first frame update
     void Start()
     {
-        audiomanager.instance.Play("MenuTheme");
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+
+        if (inMenu)
+        {
+            audiomanager.instance.Play("MenuTheme");
+        }
+        else
+        {
+            StartCoroutine("RandomSounds",2);
+        }
+        
+    }
+
+    IEnumerator RandomSounds(int delay)
+    {
+            
+            string fName = "background" + Random.Range(1, 13).ToString();
+            audiomanager.instance.Play(fName);
+            yield return new WaitForSeconds((float)delay);
+        StartCoroutine("RandomSounds", Random.Range(1, 4));
+    }
+
+    public void SetMenu(bool menu_state)
+    {
+        inMenu = menu_state;
     }
 }
