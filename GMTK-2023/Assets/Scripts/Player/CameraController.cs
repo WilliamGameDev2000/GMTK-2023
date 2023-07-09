135f0a000a7cacc9b19e847b07ef4da4b26525c9
benbenfive <benbenfive@gmail.com>
2023-07-09 02:15:57 -0400

    tv screen added

---
+++ GMTK-2023/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Material[] cardMats = new Material[5];
+    public GameObject[] pixels = new GameObject[20];
+    public static CameraController instance;
+    public float time;
+
+    void Start()
+    {
+        instance = this;
+        time = Time.time - 1;
+    }
+
+    void Update()
+    {
+        if (time + 0.1 < Time.time)
+        {
+            screenOff();
+        }
+    }
+
+    void OnMouseOver()
+    {
+        time = Time.time;
+    }
+
+    void OnMouseEnter()
+    {
+        screenOn();
+    }
+
+    public void screenOn()
+    {
+        time = Time.time;
+        int i = 0;
+        if (AIGameManager.instance.mustDraw == -1) {
+            while (AIGameManager.instance.hands[AIGameManager.instance.turn, i] != 0)
+            {
+                pixels[i].GetComponent<MeshRenderer>().material = cardMats[AIGameManager.instance.hands[AIGameManager.instance.turn, i]];
+                ++i;
+            }
+        }
+        else
+        {
+            while (AIGameManager.instance.hands[AIGameManager.instance.mustDraw, i] != 0)
+            {
+                pixels[i].GetComponent<MeshRenderer>().material = cardMats[AIGameManager.instance.hands[AIGameManager.instance.mustDraw, i]];
+                ++i;
+            }
+        }
+    }
+
+    public void screenOff()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            pixels[i].GetComponent<MeshRenderer>().material = cardMats[0];
+        }
+    }
+}
